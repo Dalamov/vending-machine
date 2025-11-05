ad0400ad6e02b1f0c2576588d94f0ad09c3bc80f
Daniella Alamo <dalamov08@gmail.com>
2025-11-05 15:57:39 +0100

    feature: add Item entity with name, price and quantity

---
+++ src/Item.php
@@ -0,0 +1,43 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Daniella\VendingMachine;
+
+class Item
+{
+    private string $name;
+    private float $price;
+    private int $quantity;
+
+    public function __construct(string $name, float $price, int $quantity)
+    {
+        $this->name = $name;
+        $this->price = $price;
+        $this->quantity = $quantity;
+    }
+
+    public function getName(): string
+    {
+        return $this->name;
+    }
+
+    public function getPrice(): float
+    {
+        return $this->price;
+    }
+
+    public function getQuantity(): int
+    {
+        return $this->quantity;
+    }
+
+    public function decreaseQuantity(): void
+    {
+        if ($this->quantity <= 0) {
+            throw new \RuntimeException("Item out of stock.");
+        }
+
+        $this->quantity--;
+    }
+}
